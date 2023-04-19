d5fb900107082a4917fef128875a2d85ef4fdbb6
Raja Malik <rajamalik2006@gmail.com>
2023-04-19 21:04:42 +0530

    Fixing run time errors

--- packages/backend/src/review/dto/create-review.input.ts
+++ packages/backend/src/review/dto/create-review.input.ts
@@ -12,38 +12,23 @@ import {
 @InputType()
 export class CreateReviewInput extends BaseModel {
   @Field(() => String, { nullable: false })
-  @IsString()
-  @IsNotEmpty()
-  @Length(0, 100)
   title: string;
 
   @Field(() => String, { nullable: true })
-  @IsString()
-  @Length(0, 500)
   content: string;
 
   @Field(() => [Number], { nullable: true })
-  @IsNumber({}, { each: true })
   photos?: number[];
 
   @Field(() => Number, { nullable: false })
-  @IsNumber()
-  @IsNotEmpty()
-  @Min(1)
-  @Max(5)
   rating: number;
 
   @Field(() => Number, { nullable: false })
-  @IsNumber()
-  @IsNotEmpty()
   restaurantId?: number;
 
   @Field(() => Number, { nullable: true })
-  @IsNumber()
   productId?: number;
 
   @Field(() => Number, { nullable: true })
-  @IsNumber()
-  @IsNotEmpty()
   authorId: number;
 }
